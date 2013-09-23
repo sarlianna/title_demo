668567a0a999951ec5469aa1a096ef734f2fe81f
zekna <zsharpsquared@gmail.com>
2013-09-22 20:42:50 -0700

    initial commit

---
+++ titledemo.asm
@@ -0,0 +1,225 @@
+  .inesprg 1
+  .ineschr 1
+  .inesmap 0
+  .inesmir 0      ;horizontal mirroring for vertical scrolling
+
+
+  .bank 0
+  .org $C000
+Reset:
+  SEI
+  CLD
+  LDX #$40
+  STX $4017       ; disable APU frame IRQ
+  LDX #$FF
+  TXS             ; set up stack
+  INX             ; x = 0
+  STX $2000       ; disable NMI by writing to PPUCTRL (clear 7th bit disables NMI)
+  STX $2001       ;disable rendering 
+  STX $4010       ;disable DMC IRQs
+
+vblankwait1:      ;we wait for vblank.  This is to give the PPU time to start up.
+  BIT $2002
+  BPL vblankwait1
+
+clrmem:           ;use the time to get RAM into a known state.
+  LDA #$00
+  STA $0000, x
+  STA $0100, x
+  STA $0300, x
+  STA $0400, x
+  STA $0500, x
+  STA $0600, x
+  STA $0700, x
+  LDA #$FE        ;$0200 will be used for sprites, which must be initialized to $FE or garbage will get rendered.
+  STA $0200, x
+  INX
+  BNE clrmem
+
+vblankwait2:      ;second vblank wait, PPU is ready afterward
+  BIT $2002
+  BPL vblankwait2
+
+LoadPalettes:
+  LDA $2002       ;reset high/low latch
+  LDA #$3F        ;point PPU to $3FOO, the palette that determines universal background color
+  STA $2006
+  LDA #$00
+  STA $2006
+  LDX #$00
+.loop:
+  LDA palette, x
+  STA $2007       ;hand off the data to the PPU.  It will handle storage and incrementing its internal pointer.
+  INX
+  CPX #$20
+  BNE loop
+
+LoadSprites:
+  LDX #$00
+.loop:
+  LDA sprites, x
+  STA $0200, x     ;We copy sprites to CPU RAM, at $0200.  These will be copied to OAM each frame using OAM_DMA($4014)
+  INX
+  CPX #$10
+  BNE loop
+
+;next is the background.  Normally you would set it by copying 4 pieces after setting the PPU address once, 
+;but we don't actually need all of the first nametable filled, because it won't be seen until after we start scrolling.
+  LDA $2002        ;reset high/low byte latch
+  LDA #$20         ;point PPU to $2000, the start of the first nametable.
+  STA $2006
+  LDA #$00
+  STA $2006
+  LDX #$00
+LoadBackground1:
+  LDA background1, x
+  STA $2007
+  INX
+  CPX #$00
+  BNE LoadBackground1
+  
+LoadBackground2:   ;we know X contains #$00 at this point
+  LDA background2, x
+  STA $2007
+  INX
+  CPX #$00
+  BNE LoadBackground2
+
+  LDA $2002       ;reset high/low latch
+  LDA #$2A        ;point PPU to $2A00, which lies halfway through nametable 2
+  STA $2006
+  LDA #$00
+  STA $2006
+LoadBackground3:
+  LDA background3, x
+  STA $2007
+  INX
+  CPX #$00
+  BNE LoadBackground3
+
+LoadBackground4:
+  LDA background4, x
+  STA $2007
+  INX
+  CPX #$00
+  BNE LoadBackground4
+
+LoadAttributes1st:    ;PPU address is currently at nametable 2's attribute data
+  LDA attribute2, x   ;so we copy its data before nametable 1's
+  STA $2007
+  INX
+  CPX #$40
+  BNE LoadAttributes1st
+
+  LDA $2002           ;reset latch
+  LDA #$23            ;point PPU to $23J0, the start of nametable 0's attribute data
+  STA $2006
+  LDA #$C0
+  STA $2006
+  LDX #$00
+LoadAttributes2nd:
+  LDA attribute1, x
+  STA $2007
+  INX
+  CPX #$40
+  BNE LoadAttributes2nd
+
+
+  LDA $2002         ;reset scroll and PPU address registers
+  LDA #$00
+  STA $2005
+  STA $2005
+  STA $2006
+  STA $2006
+
+  LDA #%10010000   ;enable NMI, assign pattern table 0 to sprites and pattern table 1 to background
+  STA $2000        ;PPUCTRL
+
+  LDA #%00011110   ;enable sprites and background, disable clipping on left side
+  STA $2001        ;PPUMASK
+
+Forever:
+  JMP Forever     ;From now on we only need to do logic during NMI.
+                  ;In a full game, putting logic here is fine. (logic unrelated to rendering)
+
+NMI:
+  LDA #$00        ;low byte of RAM address containing sprite data
+  STA $2003       ;OAM address
+  LDA #$02        ;high byte of RAM containing sprite data
+  STA $4014       ;OAM_DMA.  This will begin copying sprite data for us.
+
+;WHAT WILL HAPPEN HERE:
+;wait for sprite 0 hit, wait a set number of frames, and then write current graphics address to $2006
+;   this should happen outside vblank.  Unsure where it should go.
+;check scroll registers, and render any background data just past the seam
+;   this includes about to be shown text AND static image data (which must be written far in advance,
+;   as the image is too large to be done in one frame)
+;check scroll registers, and if we will soon overlap the static image, change its location and write
+;    new palette data for it
+;move all sprites the same distance we scrolled.  This should definitely be last, as we don't need vblank
+
+  RTI
+
+
+;;;;;;;;;;;;;
+
+  .bank 1
+  .org $E000
+palette:
+  .db $,$,$,$,  $,$,$,$,  $,$,$,$,  $,$,$,$   ;background palette
+  .db  ;sprite palette
+
+sprites:
+  .db $00, $00, $00, $00  ;sprite 0
+  .db $00, $00, $00, $00
+
+background1:
+  .db $, $, $, $, $, $, $, $, $, $, $, $, $, $, $, $, $
+  .db
+
+background2:
+  .db
+  .db
+
+background3:
+  .db
+  .db
+
+background4:
+  .db
+  .db
+
+background5:
+  .db
+  .db
+
+background6:
+  .db
+  .db
+
+background7:
+  .db
+  .db
+
+background8:
+  .db
+  .db
+
+attribute1:
+  .db
+  .db
+
+attribute2:
+  .db
+  .db
+
+  .org $FFFA
+  .dw NMI   ;address to jump to when an NMI happens
+  .dw RESET ;address to jump to on reset  or power on
+  .dw 0     ;external interrupt IRQ
+
+;;;;;;;;;;;;;;;
+
+  .bank 2
+  .org $0000
+  .incbin ""   ;graphics file to be included
